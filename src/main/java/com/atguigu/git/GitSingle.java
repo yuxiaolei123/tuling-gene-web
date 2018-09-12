e941a8d83f4f6502dd4c7dcb7a2ac2511490e88b
yuchunlei <370956351@qq.com>
2018-09-12 12:49:56 +0800

    hhh

--- src/main/java/com/atguigu/git/GitSingle.java
+++ src/main/java/com/atguigu/git/GitSingle.java
@@ -1,5 +1,17 @@
 package com.atguigu.git;
 
 public class GitSingle {
+	
+	private GitSingle(){}
+	
+	private static GitSingle single = null;
+	
+	public static GitSingle getInstance(){
+		if(single == null){
+			single = new GitSingle();
+		}
+		
+		return null;
+	}
 
 }
